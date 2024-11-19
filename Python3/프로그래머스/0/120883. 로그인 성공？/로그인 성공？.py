64ddc761e5cc12c7832194ffa9c193ed80100c5c
Donghyeon Lee <idleh4021@naver.com>
2024-11-19 14:10:54 +0900

    [level 0] Title: 로그인 성공?, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- Python3/프로그래머스/0/120883. 로그인 성공？/로그인 성공？.py
+++ Python3/프로그래머스/0/120883. 로그인 성공？/로그인 성공？.py
@@ -1,5 +1,12 @@
 def solution(id_pw, db):
-    if id_pw in db: return 'login'
+    #dict()를 이용해서 딕셔너리화 개선
+    if(dict(db).get(id_pw[0]) == id_pw[1]): return 'login'
+    elif(dict(db).get(id_pw[0])==None) : return'fail'
+    else : return 'wrong pw'
+
+    ''' 
+   기존답
+   if id_pw in db: return 'login'
     list_db = {}
     for row in db:
         list_db[row[0]] = row[1]
@@ -9,3 +16,4 @@ def solution(id_pw, db):
     
     answer = ''
     return answer
+    '''
