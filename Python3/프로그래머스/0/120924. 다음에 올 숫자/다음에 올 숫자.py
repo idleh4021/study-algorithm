78505cd4e63c90e8a05acd7d97080f62daf32483
Donghyeon Lee <idleh4021@naver.com>
2024-11-20 22:43:09 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- Python3/프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.py
+++ Python3/프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.py
@@ -1,9 +1,3 @@
 def solution(common):
     answer = 0
-    if(abs(common[0]-common[1]) == abs(common[1]-common[2])):
-        gap = common[1]-common[0]
-        answer = common[-1]+gap
-    else :
-        gap = common[1] / common[0]
-        answer = common[-1]*gap
     return answer
