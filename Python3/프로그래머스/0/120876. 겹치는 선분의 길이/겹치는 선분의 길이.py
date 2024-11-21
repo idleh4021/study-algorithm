dd2d30ea7f4d634a67e84207dcd84c12e8c08149
Donghyeon Lee <idleh4021@naver.com>
2024-11-21 23:57:09 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.09 ms, Memory: 10.2 MB -BaekjoonHub

--- Python3/프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.py
+++ Python3/프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.py
@@ -1,13 +1,12 @@
 def solution(lines):
-    # 선분을 나타낼 좌표를 저장할 배열 (101 크기, -100부터 100까지 사용 가능)
-    line_map = [0] * 201  # -100을 0에 대응시키기 위해 200 크기로 선언
-
-    # 각 선분의 시작과 끝을 표시
-    for start, end in lines:
-        for i in range(start, end):  # 구간에 해당하는 값을 증가
-            line_map[i + 100] += 1  # -100에 대한 오프셋 추가
-
-    # 두 개 이상의 선분이 겹친 부분의 길이를 계산
-    overlap_length = sum(1 for count in line_map if count >= 2)
-    
-    return overlap_length
+    list_line = []
+    for line in lines:
+        line_arr = list(range(line[0],line[1]+1))
+        list_line+=line_arr
+        #list_line.append(line_arr)
+    list_value = list(i for i in set(list_line) if len([j for j in list_line if j==i])>1)
+    cnt = 1
+    for idx in range(1,len(list_value)):
+        if(abs(abs(list_value[idx])-abs(list_value[idx-1]))==1) :cnt+=1
+    answer = cnt-1
+    return answer
