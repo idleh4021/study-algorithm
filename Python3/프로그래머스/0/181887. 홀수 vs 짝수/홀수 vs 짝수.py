9cdfd41422edb2dcf731e4330b5656d1cbcf450b
Donghyeon Lee <idleh4021@naver.com>
2024-11-22 10:35:29 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- Python3/프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.py
+++ Python3/프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.py
@@ -1,4 +1,5 @@
 def solution(num_list):
     odd =num_list[0::2]
     even = num_list[1::2]
-    return sum(odd) if sum(odd)>sum(even) else sum(even)
+    max_val = max([sum(odd),sum(even)])
+    return max_val
