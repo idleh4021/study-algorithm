cd32f0af7f79caab95605c5b882d170ad2aaf9eb
Donghyeon Lee <idleh4021@naver.com>
2024-11-26 10:37:23 +0900

    [level 0] Title: 1로 만들기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ Python3/프로그래머스/0/181880. 1로 만들기/1로 만들기.py
@@ -0,0 +1,7 @@
+def solution(num_list):
+    answer = 0
+    for num in num_list:
+        while(num != 1):
+            num = (num-(1 if num%2 == 1 else 0)) // 2
+            answer+=1
+    return answer
