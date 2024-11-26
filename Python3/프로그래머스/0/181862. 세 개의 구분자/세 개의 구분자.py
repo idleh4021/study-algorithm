e8ed7314ed47f65b34391e0e95d8c7863bb2fa6e
Donghyeon Lee <idleh4021@naver.com>
2024-11-26 15:08:26 +0900

    [level 0] Title: 세 개의 구분자, Time: 8.94 ms, Memory: 23.4 MB -BaekjoonHub

--- Python3/프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.py
+++ Python3/프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.py
@@ -1,3 +1,7 @@
 def solution(myStr):
-    answer= myStr.replace('a','c').replace('b','c').split('c')
-    return [i for i in answer if i !=''] if len([i for i in answer if i !='']) != 0 else ['EMPTY']
+    #split 매개변수 없이 처리
+    answer= myStr.replace('a',' ').replace('b',' ').replace('c',' ').split()
+    return answer if len(answer)>0 else ['EMPTY']
+    #기존답
+    #answer= myStr.replace('a','c').replace('b','c').split('c')
+    #return [i for i in answer if i !=''] if len([i for i in answer if i !='']) != 0 else ['EMPTY']
