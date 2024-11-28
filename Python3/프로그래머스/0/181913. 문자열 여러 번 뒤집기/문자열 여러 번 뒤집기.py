1601275fe58f02cea8139d1dc3f57eab6c58c84f
Donghyeon Lee <idleh4021@naver.com>
2024-11-28 10:07:24 +0900

    [level 0] Title: 문자열 여러 번 뒤집기, Time: 1.11 ms, Memory: 10.4 MB -BaekjoonHub

--- Python3/프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.py
+++ Python3/프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.py
@@ -1,8 +1,5 @@
 def solution(my_string, queries):
     answer=[]
     for q in queries:
-        result = [c for c in range(q[1],q[0]-1,-1)]
         my_string = my_string[:q[0]]+my_string[q[1]:q[0]-1 if q[0]-1>=0 else None :-1]+my_string[q[1]+1:]
-        
-        
     return my_string
