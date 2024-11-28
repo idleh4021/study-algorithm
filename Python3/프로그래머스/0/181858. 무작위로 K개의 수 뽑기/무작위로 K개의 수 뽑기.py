bc907d2a11cba045550db8768bfb90380ea99026
Donghyeon Lee <idleh4021@naver.com>
2024-11-28 10:33:26 +0900

    [level 0] Title: 무작위로 K개의 수 뽑기, Time: 64.72 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ Python3/프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.py
@@ -0,0 +1,9 @@
+def solution(arr, k):
+    answer=[]
+    for i in arr:
+        if(len(answer)==k) : break
+        if(i not in answer): answer.append(i)
+        
+    if(len(answer)<k):
+        answer+=[-1]*(k-len(answer))
+    return answer
