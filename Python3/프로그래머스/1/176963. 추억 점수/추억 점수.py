524b7312edf528f07d71fd057a5409bb8845736d
Donghyeon Lee <idleh4021@naver.com>
2024-12-04 12:31:47 +0900

    [level 1] Title: 추억 점수, Time: 9.94 ms, Memory: 10.7 MB -BaekjoonHub

--- Python3/프로그래머스/1/176963. 추억 점수/추억 점수.py
+++ Python3/프로그래머스/1/176963. 추억 점수/추억 점수.py
@@ -1,6 +1,7 @@
 def solution(name, yearning, photo):
     answer=[]
-    map_score = {key: value for key, value in zip(name,yearning)}
+    #기존답 map_score = {key: value for key, value in zip(name,yearning)}
+    map_score = dict(zip(name,yearning))
     for pic in photo:
         answer.append(sum([map_score[i] for i in pic if i in name]))
 
