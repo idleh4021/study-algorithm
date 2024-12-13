bd273c392e4b94056b7a551d4da06256e6a968ed
Donghyeon Lee <idleh4021@naver.com>
2024-12-13 15:34:26 +0900

    [level 2] Title: 짝지어 제거하기, Time: 144.17 ms, Memory: 12.2 MB -BaekjoonHub

--- Python3/프로그래머스/2/12973. 짝지어 제거하기/README.md
+++ Python3/프로그래머스/2/12973. 짝지어 제거하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 짝지어 제거하기 - 12973 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12973) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12973?language=python3) 
 
 ### 성능 요약
 
-메모리: 23.9 MB, 시간: 305.41 ms
+메모리: 12.2 MB, 시간: 144.17 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 13일 15:28:16
+2024년 12월 13일 15:34:23
 
 ### 문제 설명
 
--- Python3/프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
+++ Python3/프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -1,25 +1,11 @@
 def solution(s):
-    candidates=[]
-    s= list(s)
-    while(len(s) > 0):
-        if len(s)==0 :break
-        last = s.pop()
-        if len(candidates)>0 and candidates[-1]==last:
-            candidates.pop()
-            continue
-        if len(s)==0 : 
-            candidates.append(last)
-            break 
-        last_before = s.pop()
-        if last == last_before : continue
-        else :
-            candidates.append(last)
-            if len(s)==0 : 
-                
-                break
-            if(last_before != s[-1]):
-                candidates.append(last_before)            
-            else :        
-                if len(s)>0 : s.pop()
-                else: break
-    return int(len(s)==0 and len(candidates)==0)
+    answer =[]
+    for i in s:
+        if len(answer)==0:
+            answer.append(i)
+        elif i == answer[-1]:
+            answer.pop()
+        else : 
+            answer.append(i)          
+
+    return int(len(answer)==0)
