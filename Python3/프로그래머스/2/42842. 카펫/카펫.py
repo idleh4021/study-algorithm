64e8b9cac3a089a414d3e8822c193d565084c279
Donghyeon Lee <idleh4021@naver.com>
2024-12-14 20:16:03 +0900

    [level 2] Title: 카펫, Time: 0.86 ms, Memory: 10.2 MB -BaekjoonHub

--- Python3/프로그래머스/2/42842. 카펫/카펫.py
+++ Python3/프로그래머스/2/42842. 카펫/카펫.py
@@ -1,21 +1,6 @@
 def solution(brown, yellow):
-    
-    
+    b_val=[]
+    y_val=[]
     prod_val = brown+yellow
-    b_val = get_ms_list(prod_val)
-    br_val = get_ms_list(brown)
-    y_val = get_ms_list(yellow)
-    for b in b_val:
-        m_list=[i for i in y_val if max(b) > max(i) and min(b)> min(i)+1  ]
-        if len(m_list)>0:
-            return [max(b),min(b)]
-        
-    return y_val
-
-def get_ms_list(num):
-    answer=[]
-    if num==1 :return [[1,1]]
-    for i in range(2,int(num**0.5)+2):
-        if num%i ==0:
-            answer.append([i,num//i])
-    return answer
+   
+    return b_val
