5b5a2af491898f3e1d366a00aa82b441c5b5dfba
Donghyeon Lee <idleh4021@naver.com>
2025-08-06 09:09:01 +0900

    [Bronze II] Title: 암호, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/1718. 암호/암호.py
@@ -0,0 +1,16 @@
+alpabet = list('abcdefghijklmnopqrstuvwxyz') *2
+
+t= input()
+k = input()
+
+def encrypt(text, key):
+    result =''
+    for i,c in enumerate(text):
+        if ord(c) == 32:
+            result += c
+        else:
+            num = ord(c) - ord(key[i % len(key)]) -1
+            result += alpabet[num]
+    return result
+
+print(encrypt(t, k))
