728b815f5711796ec51e1c56aaef1cd6cb59c933
Donghyeon Lee <idleh4021@naver.com>
2025-09-10 13:44:42 +0900

    [Bronze II] Title: 맥주 냉장고, Time: 88 ms, Memory: 33432 KB -BaekjoonHub

--- Python/백준/Bronze/3595. 맥주 냉장고/맥주 냉장고.py
+++ Python/백준/Bronze/3595. 맥주 냉장고/맥주 냉장고.py
@@ -6,12 +6,18 @@ def getDiv(n):
         if n% i == 0 : arr.append(i)
     return arr
 
-def getMinBlocks(arr):
+def getSurface(arr):
     result = {}
     for i in arr:
         a,b,c = i[0],i[1],i[2]
         answer = ((a*b)+(b*c)+(a*c))*2
         result[(a,b,c)]= answer
+    return result
+
+def gap(dic):
+    result = {}
+    for i in dic:
+        result[tuple(i)] = abs(i[0]-i[1])+abs(i[1]-i[2])
     return result
 
 n = int(input())
@@ -27,7 +33,10 @@ for i in arr:
     lst.extend([[a,j,int(b//j)] for j in c_list])
     c_list2= getDiv(a)
     lst.extend([[j,b,int(a//j)] for j in c_list2])
-    answer=getMinBlocks(lst)
+    #answer=getSurface(lst)
+    answer = gap(lst)
 #print(*arr)
-print(*min(answer, key=answer.get))
+#print(answer)
+print(*min(answer,key=answer.get))
+#print(*min(answer, key=answer.get))
     
